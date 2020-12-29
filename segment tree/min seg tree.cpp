9fc451110834eb524821c5ffbbb73c69dde76d57
Aditya Girish Pawate <51478802+AdityaGirishPawate@users.noreply.github.com>
2020-12-29 16:44:42 +0530

    Update min seg tree.cpp

--- segment tree/min seg tree.cpp
+++ segment tree/min seg tree.cpp
@@ -8,14 +8,14 @@ using namespace std;
 
 struct seg_tree{
     int size=1;
-    vector<int> sums;
+    vector<int> minimum;
     void init(int n){
         while(size<n) size*=2;
-        sums.assign(2*size,0LL);
+        minimum.assign(2*size,0LL);
     }
     void set(int i, int v, int x, int lx, int rx){
         if(rx-lx==1){
-            sums[x]=v;
+            minimum[x]=v;
             return;
         }
         int m=(lx+rx)/2;
@@ -25,23 +25,23 @@ struct seg_tree{
         else{
             set(i,v,2*x+2,m,rx);
         }
-        sums[x]=min(sums[2*x+1],sums[2*x+2]);
+        minimum[x]=min(minimum[2*x+1],minimum[2*x+2]);
     }
     void set(int i,int v){
         set(i,v,0,0,size);
     }
-    int sum(int l, int r, int x, int lx, int rx){
+    int Min(int l, int r, int x, int lx, int rx){
         if(l<=lx && r>=rx){
-            return sums[x];
+            return minimum[x];
         }
         if(l>=rx || lx>=r){
             return INF;
         }
         int m=(lx + rx)/2;
-        return min(sum(l,r,2*x+1,lx,m),sum(l,r,2*x+2,m,rx));
+        return min(Min(l,r,2*x+1,lx,m),Min(l,r,2*x+2,m,rx));
     }
-    int sum(int l, int r){
-        return sum(l,r,0,0,size);
+    int Min(int l, int r){
+        return Min(l,r,0,0,size);
     }
 };
 
@@ -68,7 +68,7 @@ int32_t main() {
         else if(a==2){
             int l,r;
             cin>>l>>r;
-            cout<<st.sum(l,r)<<endl;
+            cout<<st.Min(l,r)<<endl;
         }
     }
     return 0;
