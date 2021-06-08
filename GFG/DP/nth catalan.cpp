04e725e9251b5a6cc0ec30a74f836b970ff329b6
Aditya Girish Pawate <51478802+AdityaGirishPawate@users.noreply.github.com>
2021-06-08 21:43:53 +0530

    Create nth catalan.cpp

---
+++ GFG/DP/nth catalan.cpp
@@ -0,0 +1,45 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <boost/multiprecision/cpp_int.hpp>
+using boost::multiprecision::cpp_int;  // https://www.geeksforgeeks.org/factorial-large-number-using-boost-multiprecision-library/
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+
+class Solution
+{
+    public:
+    //Function to find the nth catalan number.
+    cpp_int findCatalan(int n) 
+    {
+        cpp_int cat[n];
+        cat[0]=1;
+        for(int i=1;i<n;i++){
+            cat[i] = (cat[i-1]*(2*(2*i+1)))/(i+2);
+        }
+        return cat[n-1];
+    }
+};
+
+// { Driver Code Starts.
+
+int main() 
+{
+    //taking count of testcases
+	int t;
+	cin>>t;
+	while(t--) {
+	    
+	    //taking nth number
+	    int n;
+	    cin>>n;
+	    Solution obj;
+	    //calling function findCatalan function
+	    cout<< obj.findCatalan(n) <<"\n";    
+	}
+	return 0;
+}  // } Driver Code Ends
